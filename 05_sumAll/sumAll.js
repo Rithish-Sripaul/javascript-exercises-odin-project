3fc73f83dab70a58a09671cedf1f62b369a1e6c8
Rithish-Sripaul <rithishsripaul@gmail.com>
2022-08-20 23:16:23 +0530

    05 Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let result = 0;
 
+  if (
+    start < 0 ||
+    end < 0 ||
+    typeof start != "number" ||
+    typeof end != "number"
+  ) {
+    return "ERROR";
+  }
+
+  if (start > end) {
+    temp = start;
+    start = end;
+    end = temp;
+  }
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
