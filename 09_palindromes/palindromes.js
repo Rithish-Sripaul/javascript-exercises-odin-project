f5fb1c3d8c7d2e0321384ef94a967c03a8ee63fa
Rithish-Sripaul <rithishsripaul@gmail.com>
2022-10-13 00:11:51 +0530

    09 palindromes
    Learned how to reverse a string using split(), reverse(), and join().
    
    Also learned how to remove non alphabetic charachters from a string.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let palindromeStr = str.toLowerCase().replace(/[^a-z]/g, "");
+  return palindromeStr.split("").reverse().join("") == palindromeStr;
 };
 
 // Do not edit below this line
